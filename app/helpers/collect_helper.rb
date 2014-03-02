70e59998c742f55ce0c1fc95a62729bfc521e93a
jnf <jflores@thinkthroughmath.com>
2014-03-02 18:14:33 -0500

    Use a helper to generate varied input types based on the Question's kind attribute.

--- app/helpers/collect_helper.rb
+++ app/helpers/collect_helper.rb
@@ -1,2 +1,28 @@
 module CollectHelper
+  def input_for(response)
+    id      = "#{response.question.id}_value"
+    name    = "responses[][value]"
+    values  = response.question.values.blank? ? nil : response.question.values.split(',')
+
+    case response.question.kind
+      when "text"     then text_field_tag(name, nil, id: id)
+      when "tel"      then telephone_field_tag(name, nil, id: id)
+      when "email"    then email_field_tag(name, nil, id: id)
+      when "textarea" then text_area_tag(name, nil, id: id)
+      when "radio"    then radio_group(name, id, values)
+      else ""
+    end
+  end
+
+  protected
+
+  def radio_group(name, id, values)
+    values.each_with_index.map { |value, index|
+      indexed_id = "#{id}_#{index}"
+      button = radio_button_tag name, value, false, id: indexed_id
+      label_tag indexed_id do
+        button + value
+      end
+    }.reduce &:+
+  end
 end
