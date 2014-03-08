98f86f35ea12206c3126ef9b59b8ee781731b9c7
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-08 16:59:34 -0500

    destroys session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_url
   end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_url
+  end
 end
