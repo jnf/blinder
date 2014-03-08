53ff5917aa62b5f69eb1a4b9dac8e03be05a8a6b
Zack Mance <zmance@gmail.com>
2014-03-08 18:53:31 -0500

    [Issue 7] Adds model changes for radio groups
    Question now validates group if kind == radio
    response now strips out value correctly for radio

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,10 +1,15 @@
 class Response < ActiveRecord::Base
   belongs_to :proposal
-  belongs_to :question
+  belongs_to :question	
+  before_save { |controller|  get_value_for_radio(self.value) unless (self.value.is_a? String) }
 
   validate :required_field?
 
   protected
+
+  def get_value_for_radio(value_hash)
+  	self.value = value_hash.values.first.to_s
+  end
 
   def required_field?
     if question.required? and value.blank?
