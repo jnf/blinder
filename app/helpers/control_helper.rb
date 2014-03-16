34711d8cb3aad31acb5fb05f44e0385701fe4023
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-16 11:35:54 -0400

    adding text to control list so it is easy to tell what is active and expired

--- app/helpers/control_helper.rb
+++ app/helpers/control_helper.rb
@@ -1,5 +1,20 @@
 module ControlHelper
   def event_list(events)
     events.map { |event| content_tag :li, link_to(event.title, edit_event_path(event)) }.reduce &:+
+    events.map do |event|
+      content_tag :li, link_to( event.title, edit_event_path(event) ) + " #{event_state(event)}"
+    end.reduce &:+
+  end
+
+  def active_text event
+    'active' if event.active?
+  end
+  def expired_text event
+    'expired' if event.expired?
+  end
+
+  def event_state event
+    states = [active_text(event), expired_text(event)].compact
+    states.join ' '
   end
 end
