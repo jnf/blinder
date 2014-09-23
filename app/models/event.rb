422b4c7bd133bc55c10156bcbc10817165e34fff
jnf <jflores@thinkthroughmath.com>
2014-09-23 14:43:51 -0700

    Add validations for Event slug. Add slug to seeded Event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,17 @@
 class Event < ActiveRecord::Base
-  has_many :blinds, -> { order("position ASC") }
-  has_many :questions, through: :blinds
-  has_many :proposals
-  has_many :responses, through: :proposals
+  validates :slug, uniqueness: true, length: { in: 3..10 }, presence: true
 
-  scope :active, -> { where('active = ? AND ? <= expires_at', true, Time.now) }
-  scope :expired, -> { where('? > expires_at', Time.now) }
-  scope :inactive, -> { where(active: false) }
+  has_many :proposals
+  has_many :questions, through: :blinds
+  has_many :responses, through: :proposals
+  has_many :blinds, -> { order("position ASC") }
+
+  scope :active,    -> { where('active = ? AND ? <= expires_at', true, Time.now) }
+  scope :expired,   -> { where('? > expires_at', Time.now) }
+  scope :inactive,  -> { where(active: false) }
 
   def expired?
-    return false if expires_at.nil?
-    expires_at.past?
+    expires_at && expires_at.past?
   end
 
   def is_a_human?(key)
